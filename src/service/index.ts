ed9f24db8ae44df93bd7f7f07c773c5bcaa48f68
Ashik Rahman <ashrhmn99@gmail.com>
2021-09-21 15:33:36 +0600

    Remove test components

--- src/service/index.ts
+++ src/service/index.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const token = localStorage.getItem("authToken");
+// const token = localStorage.getItem("authToken");
 
 export default axios.create({
   baseURL: "http://localhost:5000/api/",
   headers: {
-    authToken: token,
+    authToken: localStorage.getItem("authToken"),
     "Content-type": "Application/json",
   },
 });
