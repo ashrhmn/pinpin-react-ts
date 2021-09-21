3137e13a7d32aad04df7296caf4a7d99bdf7a29c
Ashik Rahman <ashrhmn99@gmail.com>
2021-09-21 15:43:05 +0600

    Configure auto deploy to github pages

--- src/service/index.ts
+++ src/service/index.ts
@@ -3,7 +3,8 @@ import axios from "axios";
 // const token = localStorage.getItem("authToken");
 
 export default axios.create({
-  baseURL: "http://localhost:5000/api/",
+  baseURL: "https://pinash.herokuapp.com/api/",
+//   baseURL: "http://localhost:5000/api/",
   headers: {
     authToken: localStorage.getItem("authToken"),
     "Content-type": "Application/json",
